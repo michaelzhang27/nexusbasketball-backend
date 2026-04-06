a199ab5a80e2aac8da26882c6a8fe8b424cbc9d9
Michael Zhang <michaelzhang2023@gmail.com>
2026-04-05 20:51:31 -0400

    new cors endpoints

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ app = FastAPI(title="Nexus Analytics API", version="0.1.0")
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://nexusbasketball.org", "https://nexusbasketball.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
